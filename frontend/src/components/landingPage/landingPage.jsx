ed0b3903db821b0d00114660894e8cd04b207377
Reshal Maryson <reshalmaryson000@gmail.com>
2026-09-13 14:22:33 +0500

    fixed file path issue for deploment

--- frontend/src/components/landingPage/landingPage.jsx
+++ frontend/src/components/landingPage/landingPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "../../css/landingPage/landingPage.css";
+import "../../css/landingPage/landingpage.css";
 
 const HEADLINES = [
   {
